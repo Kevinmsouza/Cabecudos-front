75110aa5fab74c8879eee7c7cd71414ea36f515f
Kevinmsouza <kevinm.souza@hotmail.com>
2021-11-09 18:43:03 -0400

    Fixing env variable

--- src/services/Cabecudos.js
+++ src/services/Cabecudos.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_URL === "prod" ? "https://cabecudos-back.herokuapp.com" : "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_API_URL === "prod" ? "https://cabecudos-back.herokuapp.com" : "http://localhost:4000";
 
 function createConfig(token) {
     const config = {
